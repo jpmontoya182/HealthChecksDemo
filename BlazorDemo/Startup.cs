432f34b0cfd5c655c630f061ce702867e5ad84b5
Juan Montoya <Juan.Montoya@viamericas.com>
2020-10-20 11:58:39 -0500

    first commit

---
+++ BlazorDemo/Startup.cs
@@ -0,0 +1,84 @@
+using BlazorDemo.Data;
+using BlazorDemo.HealthChecks;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
+
+namespace BlazorDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+
+            // configure health checks
+            services.AddHealthChecks()
+                .AddCheck<ResponseTimeHealthCheck>("Network speed test", null, new[] { "service" })
+                .AddCheck<ResponseTimeHealthCheck>("Database", null, new[] { "database" })
+                .AddCheck<ResponseTimeHealthCheck>("Main Controller", null, new[] { "service" })
+                .AddCheck<ResponseTimeHealthCheck>("External Utility", null, new[] { "third" });
+
+
+            services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<ResponseTimeHealthCheck>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                // like a echo
+                endpoints.MapHealthChecks("/quickhealth", new HealthCheckOptions()
+                {
+                    Predicate = _ => false
+                });
+                // filter by tags
+                endpoints.MapHealthChecks("/health/services", new HealthCheckOptions()
+                {
+                    Predicate = reg => reg.Tags.Contains("service"),
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
+                // 
+                endpoints.MapHealthChecks("/health", new HealthCheckOptions()
+                {
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
